5efa87dad303571f351005234c5756087f4bff02
Danila Shuvaev <dshuvaev@art3d.ru>
2026-01-17 13:52:48 +0300

    Update Question.tsx

--- app/src/components/Question.tsx
+++ app/src/components/Question.tsx
@@ -1,3 +1,25 @@
-export default function Question() {
-    
+type QuestionProps = {
+    title: string;
+    variants: string[];
+}
+
+export default function Question({ title, variants }: QuestionProps) {
+    return (
+        <div className='question'>
+            <div className='question__title'>{title}</div>
+            {variants.map((variant, i) => (
+                <div
+                    key={i}
+                    className='question__variant'
+                    onClick={openVariant}
+                >
+                    {variant}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+function openVariant() {
+
 }
