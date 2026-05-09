f7de2fc191dfbbca937cdc78af8fdad3e125012a
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2026-05-09 15:21:42 +0300

    Add ability to pass the turn to the opposite command

--- app/src/components/TeamScore.tsx
+++ app/src/components/TeamScore.tsx
@@ -4,8 +4,15 @@ import { currentTeamSignal, scoreSignal } from "../store";
 export default function TeamScore({ color }: { color: TeamColor}) {
     const isActive = currentTeamSignal.value === color;
 
+    const handleClick = () => {
+        currentTeamSignal.value = currentTeamSignal.value === 'red' ? 'blue' : 'red';
+    }
+
     return (
-        <div className={`team-score ${color} ${isActive ? 'active' : ''}`}>
+        <div
+            className={`team-score ${color} ${isActive ? 'active' : ''}`}
+            onClick={() => handleClick()}
+        >
             <div className={'team-score__color-box'}></div>
             <div className={'team-score__score'}>{scoreSignal.value[color]}</div>
         </div>
